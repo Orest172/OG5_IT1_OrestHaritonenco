6cd7ba7ce8490ce186da8258bbf90fe2b5fbe4d2
BenserSPH <github.bensersph@gmail.com>
2026-09-02 20:04:16 +0200

    added user input, multiplication and finished the outputs with case

--- Taschenrechner_v0/src/git_taschenrechner/TaschenrechnerTest.java
+++ Taschenrechner_v0/src/git_taschenrechner/TaschenrechnerTest.java
@@ -19,8 +19,9 @@ public class TaschenrechnerTest {
 		System.out.println("| Options:                 |");
 		System.out.println("|        1. Addieren       |");
 		System.out.println("|        2. Subtrahieren   |");
-		System.out.println("|        3. Dividieren     |");
-		System.out.println("|        4. Exit           |");
+		System.out.println("|        3. Multiplizieren |");
+		System.out.println("|        4. Dividieren     |");
+		System.out.println("|        5. Exit           |");
 		System.out.println("============================");
 		System.out.print(" Select option: ");
 		swValue = myScanner.next().charAt(0);
@@ -28,11 +29,45 @@ public class TaschenrechnerTest {
 		// Switch construct
 		switch (swValue) {
 		case '1':
-			System.out.println("4 + 5 = " + ts.add(4, 5));
+			System.out.println("The first number you want to sum: ");
+			int addNumb1 = myScanner.nextInt();
+			
+			System.out.println("The second number you want to sum: ");
+			int addNumb2 = myScanner.nextInt();
+			
+			System.out.println(ts.add(addNumb1, addNumb2));
 			break;
 			
+		case '2':
+			System.out.println("The first number you want to sub: ");
+			int subNumb1 = myScanner.nextInt();
 			
-		  //  add your code here
+			System.out.println("The second number you want to sub: ");
+			int subNumb2 = myScanner.nextInt();
+			System.out.println(ts.sub(subNumb1, subNumb2));
+			break;
+			
+		case '3':
+			System.out.println("The first number you want to mul: ");
+			int mulNumb1 = myScanner.nextInt();
+			
+			System.out.println("The second number you want to mul: ");
+			int mulNumb2 = myScanner.nextInt();
+			System.out.println(ts.mul(mulNumb1, mulNumb2));
+			break;
+			
+		case '4':
+			System.out.println("The first number you want to div: ");
+			int divNumb1 = myScanner.nextInt();
+			
+			System.out.println("The second number you want to div: ");
+			int divNumb2 = myScanner.nextInt();
+			System.out.println(ts.add(divNumb1, divNumb2));
+			break;
+			
+		case '5':
+			System.out.println("Exit Confirmed");
+			
 		  
 		  
 		default:
