c35ec21a28fd6e924127bf09abc2bb6e79f82354
Orest <haritonencoorest@gmail.com>
2026-09-09 12:43:51 +0200

    4. "Button Verbrauchen" function added now "fuellstand" - 2 when button clicked

--- Tanksimulator/src/de/futurehome/tanksimulator/MyActionListener.java
+++ Tanksimulator/src/de/futurehome/tanksimulator/MyActionListener.java
@@ -21,6 +21,12 @@ public class MyActionListener implements ActionListener {
 
 			 f.lblFuellstand.setText(""+fuellstand);
 		}
+		
+		if (obj == f.btnVerbrauchen) {
+			double fuellstand = f.myTank.getFuellstand();
+			fuellstand = fuellstand - 2;
+			f.myTank.setFuellstand(fuellstand);
+		}
 
 	}
 }
