a7cc36b7d0c8e3afd1bebbe6fd329c9678fc4630
Orest <haritonencoorest@gmail.com>
2026-09-16 13:02:43 +0200

    Pilot attribute, methoden hinzugefügt

--- StarSim2099/src/de/oszimt/starsim2099/Pilot.java
+++ StarSim2099/src/de/oszimt/starsim2099/Pilot.java
@@ -10,6 +10,46 @@ public class Pilot {
 
 	// Attribute
 	
+	private String name;
+	private String grad;
+	private double posX;
+	private double posY;
+	
 	// Methoden
-
+	
+	public Pilot() {
+		
+	}
+	
+	public void setName(String n) {
+		this.name = n;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public void setGrad(String g) {
+		this.grad = g;
+	}
+	
+	public String getGrad() {
+		return this.grad;
+	}
+	
+	public void setPosX(double p) {
+		this.posX = p;
+	}
+	
+	public double getPosX() {
+		return this.posX;
+	}
+	
+	public void setPosY(double p) {
+		this.posY = p;
+	}
+	
+	public double getPosY() {
+		return this.posY;
+	}
 }
