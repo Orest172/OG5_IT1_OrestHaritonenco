1b7e3b1ca838c552a56bfe88e9cdea2af437f61c
Orest <haritonencoorest@gmail.com>
2026-09-17 07:52:09 +0200

    Ladung implementiert: Attribute, Methoden

--- StarSim2099/src/de/oszimt/starsim2099/Ladung.java
+++ StarSim2099/src/de/oszimt/starsim2099/Ladung.java
@@ -10,8 +10,45 @@ public class Ladung {
 
 	// Attribute
 
+	private String typ;
+	private int masse;
+	private double posX;
+	private double posY;
+	
 	// Methoden
 
+	public Ladung() {
+		
+	}
+	
+	public void setTyp(String t) {
+		this.typ = t;
+	}
+	public String getTyp() {
+		return this.typ;
+	}
+	
+	public void setMasse(int m) {
+		this.masse = m;
+	}
+	public int getMasse() {
+		return this.masse;
+	}
+	
+	public void setPosX(double pX) {
+		this.posX = pX;
+	}
+	public double getPosX() {
+		return this.posX;
+	}
+	
+	public void setPosY(double pY) {
+		this.posY = pY;
+	}
+	public double getPosY() {
+		return this.posY;
+	}
+	
 	// Darstellung
 	public static char[][] getDarstellung() {
 		char[][] ladungShape = { { '/', 'X', '\\' }, { '|', 'X', '|' }, { '\\', 'X', '/' } };
