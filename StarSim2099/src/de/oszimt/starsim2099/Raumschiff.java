52fd5e0a992f30ffe1ffc13ddd473e819fc1446c
BenserSPH <haritonencoorest@gmail.com>
2026-09-17 08:00:20 +0200

    Raumschiff implementiert: Konstruktor, Attribute, Verwaltungsmethoden

--- StarSim2099/src/de/oszimt/starsim2099/Raumschiff.java
+++ StarSim2099/src/de/oszimt/starsim2099/Raumschiff.java
@@ -10,8 +10,61 @@ public class Raumschiff {
 
 	// Attribute
 	
+	private String typ;
+	private String antrieb;
+	private int winkel;
+	private int maxKapazitaet;
+	private double posX;
+	private double posY;
+	
 	// Methoden
 
+	public Raumschiff() {
+		
+	}
+	
+	public void setTyp(String t) {
+		this.typ = t;
+	}
+	public String getTyp() {
+		return this.typ;
+	}
+	
+	public void setAntrieb(String a) {
+		this.antrieb = a;
+	}
+	public String getAntrieb() {
+		return this.antrieb;
+	}
+	
+	public void setWinkel(int w) {
+		this.winkel = w;
+	}
+	public int getWinkel() {
+		return this.winkel;
+	}
+	
+	public void setMaxLadekapazitaet(int mLk) {
+		this.maxKapazitaet = mLk;
+	}
+	public int getMaxLadekapazitaet() {
+		return this.maxKapazitaet;
+	}
+	
+	public void setPosX(double pX) {
+		this.posX = pX;
+	}
+	public double getPosX() {
+		return this.posX;
+	}
+	
+	public void setPosY(double pY) {
+		this.posY = pY;
+	}
+	public double getPosY() {
+		return this.posY;
+	}
+	
 	// Darstellung
 	public static char[][] getDarstellung() {
 		char[][] raumschiffShape = { 
