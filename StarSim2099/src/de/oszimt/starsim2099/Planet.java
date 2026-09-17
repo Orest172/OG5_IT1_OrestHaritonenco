80e25699139cba369593e5608d46afb467c9fc63
Orest <haritonencoorest@gmail.com>
2026-09-17 07:38:10 +0200

    implementierung Planet: Attribute, Verwaltungsmethoden

--- StarSim2099/src/de/oszimt/starsim2099/Planet.java
+++ StarSim2099/src/de/oszimt/starsim2099/Planet.java
@@ -10,8 +10,45 @@ public class Planet {
 
 	// Attribute
 	
+	private String name;
+	private int anzahlHafen;
+	private double posX;
+	private double posY;
+	
 	// Methoden
 
+	public Planet() {
+		
+	}
+	
+	public void setName(String n) {
+		this.name = n;
+	}
+	public String getName() {
+		return this.name;
+	}
+	
+	public void setAnzahlHafen(int a) {
+		this.anzahlHafen = a;
+	}
+	public int getAnzahlHafen() {
+		return this.anzahlHafen;
+	}
+	
+	public void setPosX(double pX) {
+		this.posX = pX;
+	}
+	public double getPosX() {
+		return this.posX;
+	}
+	
+	public void setPosY(double pY) {
+		this.posY = pY;
+	}
+	public double getPosY() {
+		return this.posY;
+	}
+	
 	// Darstellung
 	public static char[][] getDarstellung() {
 		char[][] planetShape = { { '\0', '/', '*', '*', '\\', '\0' }, { '|', '*', '*', '*', '*', '|' },
